ebbcbbe440da880a06f5453df02a1b72891763b5
da-z <andreizinca@gmail.com>
2023-12-18 08:59:42 +0100

    chore: compare with SB 2.x

--- src/main/java/com/example/TestEntity.java
+++ src/main/java/com/example/TestEntity.java
@@ -3,6 +3,9 @@ package com.example;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+
 @Entity
 public class TestEntity {
 
